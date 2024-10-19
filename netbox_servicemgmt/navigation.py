d402fa0d5c006e5a74dc64b9db0038c97c207df5
Eric Hester <eric.hester@umbrella.tech>
2024-10-19 16:43:59 -0400

    organized menu

--- netbox_servicemgmt/navigation.py
+++ netbox_servicemgmt/navigation.py
@@ -1,6 +1,6 @@
 from netbox.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 
-items = (
+solution_items = (
     # Solution Template Menu Item
     PluginMenuItem(
         link="plugins:netbox_servicemgmt:solutiontemplate_list",
@@ -18,40 +18,6 @@ items = (
             ),
         ]
     ),
-    # Service Template Menu Item
-    PluginMenuItem(
-        link="plugins:netbox_servicemgmt:servicetemplate_list",
-        link_text="Service Templates",
-        buttons=[
-            PluginMenuButton(
-                link="plugins:netbox_servicemgmt:servicetemplate_add",
-                title="Add",
-                icon_class="mdi mdi-plus-thick",
-            ),
-            PluginMenuButton(
-                link="plugins:netbox_servicemgmt:servicetemplate_bulk_import",
-                title="Import",
-                icon_class="mdi mdi-upload",
-            ),
-        ]
-    ),
-    # Service Requirement Menu Item
-    PluginMenuItem(
-        link="plugins:netbox_servicemgmt:servicerequirement_list",
-        link_text="Service Requirements",
-        buttons=[
-            PluginMenuButton(
-                link="plugins:netbox_servicemgmt:servicerequirement_add",
-                title="Add",
-                icon_class="mdi mdi-plus-thick",
-            ),
-            PluginMenuButton(
-                link="plugins:netbox_servicemgmt:servicerequirement_bulk_import",
-                title="Import",
-                icon_class="mdi mdi-upload",
-            ),
-        ]
-    ),
     # Solution Deployment Menu Item
     PluginMenuItem(
         link="plugins:netbox_servicemgmt:solutiondeployment_list",
@@ -64,6 +30,25 @@ items = (
             ),
             PluginMenuButton(
                 link="plugins:netbox_servicemgmt:solutiondeployment_bulk_import",
+                title="Import",
+                icon_class="mdi mdi-upload",
+            ),
+        ]
+    )
+)
+    # Service Template Menu Item
+service_items = ( 
+    PluginMenuItem(
+        link="plugins:netbox_servicemgmt:servicerequirement_list",
+        link_text="Service Requirements",
+        buttons=[
+            PluginMenuButton(
+                link="plugins:netbox_servicemgmt:servicerequirement_add",
+                title="Add",
+                icon_class="mdi mdi-plus-thick",
+            ),
+            PluginMenuButton(
+                link="plugins:netbox_servicemgmt:servicerequirement_bulk_import",
                 title="Import",
                 icon_class="mdi mdi-upload",
             ),
@@ -103,10 +88,11 @@ items = (
             ),
         ]
     ),
-    # Fault Tolerence Menu Item
+)
+profile_items = (
     PluginMenuItem(
         link="plugins:netbox_servicemgmt:faulttolerance_list",
-        link_text="Fault Tolerance Models",
+        link_text="Fault Tolerance Profiles",
         buttons=[
             PluginMenuButton(
                 link="plugins:netbox_servicemgmt:faulttolerance_add",
@@ -123,7 +109,7 @@ items = (
     # SLO Menu Item
     PluginMenuItem(
         link="plugins:netbox_servicemgmt:slo_list",
-        link_text="Service Level Objectives",
+        link_text="SLO Profiles",
         buttons=[
             PluginMenuButton(
                 link="plugins:netbox_servicemgmt:slo_add",
@@ -142,6 +128,6 @@ items = (
 # Define the top-level menu with icon
 menu = PluginMenu(
     label="Service Management",
-    groups=(("SERVICE MANAGEMENT", items),),
+    groups=(("SOLUTIONS", solution_items),("SERVICES", service_items),("PROFILES", profile_items)),
     icon_class="mdi mdi-server",
 )
