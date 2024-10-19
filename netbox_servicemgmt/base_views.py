be049ed72d0a1cc76837023b287b297e4d7774d7
Eric Hester <eric.hester@umbrella.tech>
2024-10-19 17:14:15 -0400

    .

--- netbox_servicemgmt/base_views.py
+++ netbox_servicemgmt/base_views.py
@@ -34,7 +34,7 @@ class BaseObjectView(generic.ObjectView):
         field_data = []
         for field in instance._meta.get_fields():      
             # Skip excluded fields
-            if field.name in excluded_extras and field.verbose_name: 
+            if field.name in excluded_extras or not field.verbose_name: 
                 continue      
             value = None
             url = None
