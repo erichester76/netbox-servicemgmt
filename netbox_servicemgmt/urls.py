1aa7ead0aab67301159478a220eb4e7514531709
Eric Hester <eric.hester@umbrella.tech>
2024-10-19 19:33:45 -0400

    fixed changelogs

--- netbox_servicemgmt/urls.py
+++ netbox_servicemgmt/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views, models
 
 urlpatterns = [
     # Solution Template URLs
@@ -9,7 +9,7 @@ urlpatterns = [
     path('solution-templates/<int:pk>/edit/', views.SolutionTemplateEditView.as_view(), name='solutiontemplate_edit'),
     path('solution-templates/<int:pk>/delete/', views.SolutionTemplateDeleteView.as_view(), name='solutiontemplate_delete'),
     path('solution-templates/bulk-import/', views.SolutionTemplateBulkImportView.as_view(), name='solutiontemplate_bulk_import'),
-    path('solution-templates/<int:pk>/changelog/', views.SolutionTemplateChangeLogView.as_view(), name='solutiontemplate_changelog'),
+    path('solution-templates/<int:pk>/changelog/', views.SolutionTemplateChangeLogView.as_view(), name='solutiontemplate_changelog', kwargs={'model': models.SolutionTemplate}),
 
     # Service Template URLs
     path('service-templates/', views.ServiceTemplateListView.as_view(), name='servicetemplate_list'),
@@ -18,7 +18,7 @@ urlpatterns = [
     path('service-templates/<int:pk>/edit/', views.ServiceTemplateEditView.as_view(), name='servicetemplate_edit'),
     path('service-templates/<int:pk>/delete/', views.ServiceTemplateDeleteView.as_view(), name='servicetemplate_delete'),
     path('service-templates/bulk-import/', views.ServiceTemplateBulkImportView.as_view(), name='servicetemplate_bulk_import'),
-    path('service-templates/<int:pk>/changelog/', views.ServiceTemplateChangeLogView.as_view(), name='servicetemplate_changelog'),
+    path('service-templates/<int:pk>/changelog/', views.ServiceTemplateChangeLogView.as_view(), name='servicetemplate_changelog', kwargs={'model': models.ServiceTemplate}),
 
     # Service Requirement URLs
     path('service-requirements/', views.ServiceRequirementListView.as_view(), name='servicerequirement_list'),
@@ -27,7 +27,7 @@ urlpatterns = [
     path('service-requirements/<int:pk>/edit/', views.ServiceRequirementEditView.as_view(), name='servicerequirement_edit'),
     path('service-requirements/<int:pk>/delete/', views.ServiceRequirementDeleteView.as_view(), name='servicerequirement_delete'),
     path('service-requirements/bulk-import/', views.ServiceRequirementBulkImportView.as_view(), name='servicerequirement_bulk_import'),
-    path('service-requirements/<int:pk>/changelog/', views.ServiceRequirementChangeLogView.as_view(), name='servicerequirement_changelog'),
+    path('service-requirements/<int:pk>/changelog/', views.ServiceRequirementChangeLogView.as_view(), name='servicerequirement_changelog', kwargs={'model': models.ServiceRequirement}),
 
     # Solution Deployment URLs
     path('solution-deployments/', views.SolutionDeploymentListView.as_view(), name='solutiondeployment_list'),
@@ -36,7 +36,7 @@ urlpatterns = [
     path('solution-deployments/<int:pk>/edit/', views.SolutionDeploymentEditView.as_view(), name='solutiondeployment_edit'),
     path('solution-deployments/<int:pk>/delete/', views.SolutionDeploymentDeleteView.as_view(), name='solutiondeployment_delete'),
     path('solution-deployments/bulk-import/', views.SolutionDeploymentBulkImportView.as_view(), name='solutiondeployment_bulk_import'),
-    path('solution-deployments/<int:pk>/changelog/', views.SolutionDeploymentChangeLogView.as_view(), name='solutiondeployment_changelog'),
+    path('solution-deployments/<int:pk>/changelog/', views.SolutionDeploymentChangeLogView.as_view(), name='solutiondeployment_changelog', kwargs={'model': models.SolutionDeployment}),
 
     # Service Deployment URLs
     path('service-deployments/', views.ServiceDeploymentListView.as_view(), name='servicedeployment_list'),
@@ -45,7 +45,7 @@ urlpatterns = [
     path('service-deployments/<int:pk>/edit/', views.ServiceDeploymentEditView.as_view(), name='servicedeployment_edit'),
     path('service-deployments/<int:pk>/delete/', views.ServiceDeploymentDeleteView.as_view(), name='servicedeployment_delete'),
     path('service-deployments/bulk-import/', views.ServiceDeploymentBulkImportView.as_view(), name='servicedeployment_bulk_import'),
-    path('service-deployments/<int:pk>/changelog/', views.ServiceDeploymentChangeLogView.as_view(), name='servicedeployment_changelog'),
+    path('service-deployments/<int:pk>/changelog/', views.ServiceDeploymentChangeLogView.as_view(), name='servicedeployment_changelog', kwargs={'model': models.ServiceDeployment}),
 
     # Service Component URLs
     path('service-components/', views.ServiceComponentListView.as_view(), name='servicecomponent_list'),
@@ -54,7 +54,7 @@ urlpatterns = [
     path('service-components/<int:pk>/edit/', views.ServiceComponentEditView.as_view(), name='servicecomponent_edit'),
     path('service-components/<int:pk>/delete/', views.ServiceComponentDeleteView.as_view(), name='servicecomponent_delete'),
     path('service-components/bulk-import/', views.ServiceComponentBulkImportView.as_view(), name='servicecomponent_bulk_import'),
-    path('service-components/<int:pk>/changelog/', views.ServiceComponentChangeLogView.as_view(), name='servicecomponent_changelog'),
+    path('service-components/<int:pk>/changelog/', views.ServiceComponentChangeLogView.as_view(), name='servicecomponent_changelog', kwargs={'model': models.ServiceComponent}),
 
     # High Availability (HA) Model URLs
     path('fault-tolerance/', views.FaultToleranceListView.as_view(), name='faulttolerance_list'),
@@ -63,7 +63,7 @@ urlpatterns = [
     path('fault-tolerance/<int:pk>/edit/', views.FaultToleranceEditView.as_view(), name='faulttolerance_edit'),
     path('fault-tolerance/<int:pk>/delete/', views.FaultToleranceDeleteView.as_view(), name='faulttolerance_delete'),
     path('fault-tolerance/bulk-import/', views.FaultToleranceBulkImportView.as_view(), name='faulttolerance_bulk_import'),
-    path('fault-tolerance/<int:pk>/changelog/', views.FaultToleranceChangeLogView.as_view(), name='faulttolerance_changelog'),
+    path('fault-tolerance/<int:pk>/changelog/', views.FaultToleranceChangeLogView.as_view(), name='faulttolerance_changelog', kwargs={'model': models.FaultTolerence}),
 
     # Service Level Objective (SLO) URLs
     path('slos/', views.SLOListView.as_view(), name='slo_list'),
@@ -72,5 +72,5 @@ urlpatterns = [
     path('slos/<int:pk>/edit/', views.SLOEditView.as_view(), name='slo_edit'),
     path('slos/<int:pk>/delete/', views.SLODeleteView.as_view(), name='slo_delete'),
     path('slos/bulk-import/', views.SLOBulkImportView.as_view(), name='slo_bulk_import'),
-    path('slos/<int:pk>/changelog/', views.SLOChangeLogView.as_view(), name='slo_changelog'),
+    path('slos/<int:pk>/changelog/', views.SLOChangeLogView.as_view(), name='slo_changelog',  kwargs={'model': models.SLO}),
 ]
