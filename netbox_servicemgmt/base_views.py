9f37a8f762cfe6c9bacca65eeeb6e176870daf60
Eric Hester <eric.hester@umbrella.tech>
2024-11-20 08:25:31 -0500

    .

--- netbox_servicemgmt/base_views.py
+++ netbox_servicemgmt/base_views.py
@@ -184,7 +184,7 @@ def generate_mermaid_code(obj, visited=None, depth=0):
                     related_obj_id = f"{related_obj._meta.model_name}_{related_obj.pk}"
                     related_obj_name = sanitize_name(str(related_obj))  # Sanitize the related object name
                     indent = "    " * (depth+1)
-                    mermaid_code += f"{indent}{related_obj_id}({related_obj._meta.model_name} {related_obj_name}):::color_{related_obj._meta.model_name.lower()}\n"
+                    mermaid_code += f"{indent}{related_obj_id}({related_obj._meta.model_name}: {related_obj_name}):::color_{related_obj._meta.model_name.lower()}\n"
                     if hasattr(related_obj, 'get_absolute_url'):
                         mermaid_code += f'{indent}click {related_obj_id} "{related_obj.get_absolute_url()}"\n'
                     mermaid_code += f"{indent}{obj_id} --> {related_obj_id}\n"
@@ -202,7 +202,7 @@ def generate_mermaid_code(obj, visited=None, depth=0):
                 related_obj_name = sanitize_name(str(related_obj))  # Sanitize the related object name
                 # Add relationship and recurse with indent for readability
                 indent = "    " * (depth+1)
-                mermaid_code += f"{indent}{related_obj_id}({field.name} {related_obj_name}):::color_{related_obj._meta.model_name.lower()}\n"
+                mermaid_code += f"{indent}{related_obj_id}({field.name}: {related_obj_name}):::color_{related_obj._meta.model_name.lower()}\n"
                 mermaid_code += f"{indent}{obj_id} --> {related_obj_id}\n"
                 mermaid_code += generate_mermaid_code(related_obj, visited, depth + 1)
       
