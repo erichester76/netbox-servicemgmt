7725c0a9f9af2e1f2a402b67d72a75afb941df05
Eric Hester <eric.hester@umbrella.tech>
2024-11-19 21:58:07 -0500

    .

--- netbox_servicemgmt/base_views.py
+++ netbox_servicemgmt/base_views.py
@@ -200,7 +200,7 @@ def generate_mermaid_code(obj, depth=0):
                 # Add relationship and recurse with indent for readability
                 indent = "    " * (depth+1)
                 mermaid_code += f"{indent}{related_obj_id}({related_obj_name}):::color_{related_obj._meta.model_name.lower()}\n"
-                mermaid_code += f"{indent}{obj_id} --> {related_obj_id}\n"
+                mermaid_code += f"{indent}{related_obj_id} --> {obj_id}\n"
                 mermaid_code += generate_mermaid_code(related_obj, depth + 1)
       
         elif field.is_relation and field.auto_created and not field.concrete:
@@ -215,7 +215,7 @@ def generate_mermaid_code(obj, depth=0):
                     mermaid_code += f"{indent}{related_obj_id}({related_obj_name}):::color_{related_obj._meta.model_name.lower()}\n"
                     if hasattr(related_obj, 'get_absolute_url'):
                         mermaid_code += f'{indent}click {related_obj_id} "{related_obj.get_absolute_url()}"\n'
-                    mermaid_code += f"{indent}{related_obj_id} --> {obj_id}\n"
+                    mermaid_code += f"{indent}{obj_id} --> {related_obj_id}\n"
                     mermaid_code += generate_mermaid_code(related_obj, depth + 1)
     
     return mermaid_code
