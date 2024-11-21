25bf7d857b7f3f1fda69327262973b47feee2554
Eric Hester <eric.hester@umbrella.tech>
2024-11-20 23:43:40 -0500

    .

--- netbox_servicemgmt/base_views.py
+++ netbox_servicemgmt/base_views.py
@@ -206,7 +206,8 @@ def generate_mermaid_code(obj, visited=None, link_counter=0, link_styles={}, dep
                         mermaid_code += f'{indent}click {related_obj_id} "{related_obj.get_absolute_url()}"\n'
                     mermaid_code += f"{indent}{obj_id} ----- {related_obj_id}\n"
                     link_counter += 1
-                    #mermaid_code += f"linkStyle {link_counter} stroke:{color_map[obj._meta.model_name.lower()]},stroke-width:2px;\n"
+                    if obj._meta.model_name.lower() in color_map:
+                        mermaid_code += f"linkStyle {link_counter} stroke:{color_map[obj._meta.model_name.lower()]},stroke-width:2px;\n"
                     mermaid_code += generate_mermaid_code(related_obj, visited, link_counter, link_styles, depth + 1)
                 except related_model.DoesNotExist:
                     continue  # If the related object doesn't exist, skip it
