f6723a26170b599165ddedf577da46a2344bdee4
Eric Hester <eric.hester@umbrella.tech>
2024-11-20 22:18:51 -0500

    colors

--- netbox_servicemgmt/base_views.py
+++ netbox_servicemgmt/base_views.py
@@ -205,7 +205,7 @@ def generate_mermaid_code(obj, visited=None, link_counter=0, link_styles={}, dep
                     if hasattr(related_obj, 'get_absolute_url'):
                         mermaid_code += f'{indent}click {related_obj_id} "{related_obj.get_absolute_url()}"\n'
                     mermaid_code += f"{indent}{obj_id} ---- {related_obj_id}\n"
-                    #mermaid_code += f"linkStyle {link_counter} stroke:{color_map[obj._meta.model_name.lower()]},stroke-width:2px;\n"
+                    mermaid_code += f"{indent}linkStyle {link_counter} stroke:{color_map[obj._meta.model_name.lower()]},stroke-width:2px;\n"
                     link_counter += 1
                     mermaid_code += generate_mermaid_code(related_obj, visited, link_counter, link_styles, depth + 1)
                 except related_model.DoesNotExist:
@@ -223,7 +223,7 @@ def generate_mermaid_code(obj, visited=None, link_counter=0, link_styles={}, dep
                 indent = "    " * (depth+1)
                 mermaid_code += f"{indent}{related_obj_id}({field.name}: {related_obj_name}):::color_{related_obj._meta.model_name.lower()}\n"
                 mermaid_code += f"{indent}{obj_id} ---- {related_obj_id}\n"
-                #mermaid_code += f"linkStyle {link_counter} stroke:{color_map[obj._meta.model_name.lower()]},stroke-width:2px;\n"
+                mermaid_code += f"{indent}linkStyle {link_counter} stroke:{color_map[obj._meta.model_name.lower()]},stroke-width:2px;\n"
                 link_counter += 1
                 mermaid_code += generate_mermaid_code(related_obj, visited, link_counter, link_styles, depth + 1)
       
@@ -240,7 +240,7 @@ def generate_mermaid_code(obj, visited=None, link_counter=0, link_styles={}, dep
                     if hasattr(related_obj, 'get_absolute_url'):
                         mermaid_code += f'{indent}click {related_obj_id} "{related_obj.get_absolute_url()}"\n'
                     mermaid_code += f"{indent}{obj_id} ---- {related_obj_id}\n"
-                    #mermaid_code += f"linkStyle {link_counter} stroke:{color_map[obj._meta.model_name.lower()]},stroke-width:2px;\n"
+                    mermaid_code += f"{indent}linkStyle {link_counter} stroke:{color_map[obj._meta.model_name.lower()]},stroke-width:2px;\n"
                     link_counter += 1
                     mermaid_code += generate_mermaid_code(related_obj, visited, link_counter, link_styles, depth + 1)
     
