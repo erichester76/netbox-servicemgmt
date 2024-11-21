0d1d15129d02bf7986e105b3434802bfd0b21c26
Eric Hester <eric.hester@umbrella.tech>
2024-11-20 21:59:52 -0500

    .

--- netbox_servicemgmt/base_views.py
+++ netbox_servicemgmt/base_views.py
@@ -260,7 +260,7 @@ class BaseDiagramView(generic.ObjectView):
     
     def get_extra_context(self, request, instance):
         
-        mermaid_source = "%%{ init: { 'flowchart': { 'curve': 'basis' } } }%%\n"
+        mermaid_source = "%%{ init: { 'flowchart': { 'curve': 'stepBefore' } } }%%\n"
         mermaid_source += "graph LR\n" 
         #recurse object relationships to build flowchart diagram
         mermaid_source += generate_mermaid_code(instance)
