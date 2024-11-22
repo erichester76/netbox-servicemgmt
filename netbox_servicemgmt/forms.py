999eba63ff5ed1c5e6f7efe994dce418abadadbf
Eric Hester <eric.hester@umbrella.tech>
2024-11-22 06:55:16 -0500

    .

--- netbox_servicemgmt/forms.py
+++ netbox_servicemgmt/forms.py
@@ -136,7 +136,7 @@ class ServiceComponentForm(NetBoxModelForm):
     )
 
     object_id = DynamicModelChoiceField(
-        queryset=None,  # Set dynamically via JavaScript and `widget_filter`
+        queryset=ContentType.objects.all(),  # Set dynamically via JavaScript and `widget_filter`
         required=True,
         label="Component",
     )
