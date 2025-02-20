2584b1997f9882d99436cecd67b6751b39b074a6
Eric Hester <eric.hester@umbrella.tech>
2025-02-19 19:30:41 -0500

    .

--- netbox_servicemgmt/api/serializers.py
+++ netbox_servicemgmt/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import SLO, FaultTolerence, SolutionBase, Solution, Deployment, Component
+from ..models import SLO, FaultTolerence, Solution, Deployment, Component
 
 class SLOSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,48 +11,19 @@ class FaultTolerenceSerializer(serializers.ModelSerializer):
         model = FaultTolerence
         fields = '__all__'
 
-class SolutionBaseSerializer(serializers.ModelSerializer):
+class SolutionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = SolutionBase
-        fields = [
-            'name', 'solution_number', 'project_id', 'description', 'solution_type', 'version', 
-            'architect', 'requester', 'business_owner_group', 'business_owner_contact',
-            'os_technical_contact_group', 'os_technical_contact', 'app_technical_contact_group', 
-            'app_technical_contact', 'incident_contact', 'data_classification', 
-            'compliance_requirements', 'fault_tolerence', 'slos', 'last_bcdr_test', 
-            'last_risk_assessment', 'last_review', 'production_readiness_status', 
-            'vendor_management_status', 'status'
-        ]
-
-class SolutionSerializer(SolutionBaseSerializer):
-    previous_version = serializers.PrimaryKeyRelatedField(
-        queryset=Solution.objects.all(),
-        allow_null=True,
-        required=False
-    )
-    
-    class Meta(SolutionBaseSerializer.Meta):
         model = Solution
-        fields = SolutionBaseSerializer.Meta.fields + ['previous_version']
+        fields = '__all__'
 
-class DeploymentSerializer(SolutionBaseSerializer):
-    deployment_solution = serializers.PrimaryKeyRelatedField(
-        queryset=Solution.objects.all(),
-        allow_null=True,
-        required=False
-    )
-    previous_version = serializers.PrimaryKeyRelatedField(
-        queryset=Deployment.objects.all(),
-        allow_null=True,
-        required=False
-    )
+class DeploymentSerializer(serializers.ModelSerializer):
 
-    class Meta(SolutionBaseSerializer.Meta):
+    class Meta:
         model = Deployment
-        fields = SolutionBaseSerializer.Meta.fields + ['deployment_type', 'deployment_solution', 'previous_version']
+        fields = '__all__'
 
 
-class ComponentSerializer(SolutionBaseSerializer):
+class ComponentSerializer(serializers.ModelSerializer):
     content_object = serializers.SerializerMethodField()
     
     def get_content_object(self, obj):
